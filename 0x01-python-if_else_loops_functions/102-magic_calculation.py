c1a73430a0a84d07c65329c46b40584e1fc90f42
emlincs <emlincs09@gmail.com>
2023-12-06 03:02:22 -0800

    Python function def magic_calculation(a, b, c): that does exactly the same as the following Python bytecode

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided by ALX."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
